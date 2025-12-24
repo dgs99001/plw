460d3c869458372f9cf7a747098f76399655fd1f
dgs99001 <dgs99001@proton.me>
2025-12-25 00:12:05 +0800

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -160,7 +160,7 @@ function App() {
       name: '胜天国际 ',
       description: '30号正式开业，可优先注册领取百万红包',
       iconUrl: 'https://pic1.imgdb.cn/item/6937c17f00233646958cd102.jpg',
-      appUrl: 'https://bakmry.gziasti.com/app/register.php?site_id=1050&topId=880722&selfPlanId=855049'
+      appUrl: 'https://tfajn.hcjwj.com/app/register.php?site_id=1058&topId=26442&selfPlanId=1692'
     },
   ]);
 
