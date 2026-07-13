eff958589f68f6f5fd4ee5a41e0a0daa9a989c0a
dgs99001 <dgs99001@proton.me>
2026-07-13 19:25:46 +0800

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -182,7 +182,7 @@ function App() {
     {
       id: 1,
       imageUrl: 'https://pic.imgdb.cn/i/033oY29bM71ksG96Ei6IgX.jpg',
-      linkUrl: 'https：//t.me/ty2299'
+      linkUrl: 'https://t.me/ty2299'
     }, 
     {
       id: 2,
